5c2bd46069eeb15f400f18ab21d14d1c3750905e
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 20:17:47 +0200

    models

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,1 +1,13 @@
-from .models import Book, Librarian, Library
+from .models import Book, Librarian, Library, Author
+
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = author.objects.filter(author=author)
+
+def books_in_library(library_name):
+    library_books = Library.objects.get(name=library_name)
+    books = books.all()    
+
+def get_librarian(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
