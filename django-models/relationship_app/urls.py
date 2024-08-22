b711f12392333544a8cdafe2581cb5593255149e
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 21:13:40 +0200

    views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,1 +1,5 @@
-from .views import list_books, LibraryDetailView, path
+from .views import list_books, LibraryDetailView
+
+views.register 
+LogoutView.as_view(template_name= logout)
+LoginView.as_view(template_name= login)
