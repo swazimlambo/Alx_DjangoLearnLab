1f21e7a68262f4e7522edddc0b9f74f8bbc311a8
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 21:18:53 +0200

    view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -26,6 +26,7 @@ class LibraryDetailView(DetailView):
     context['average_rating'] = book.get_average_rating() 
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
-    template_name = 'relationship_app/registration.html'
+    template_name = 'relationship_app/register.html'
+  
