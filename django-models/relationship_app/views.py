7018cfeb3a2792b975c452c85f73a69d8ff7f118
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 21:55:04 +0200

    permissions

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import user_passes_test
 
 def list_books(request):
       """Retrieves all books and renders a template displaying the list."""
@@ -29,12 +30,15 @@ class SignUpView(CreateView):
     form_class = UserCreationForm()
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
-  
+
+@user_passes_test  
 def admin_view(request):
     pass
 
+@user_passes_test
 def member_view(request):
     pass
 
+@user_passes_test
 def librarian_view(request):
     pass
