0c9d00e4557f7483161291a0ba8a633a86f1463e
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 20:35:45 +0200

    library

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,20 @@
+from django.views.generic import DetailView
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
 
 def book_list(request):
       """Retrieves all books and renders a template displaying the list."""
       books = Book.objects.all()  # Fetch all book instances from the database
       context = {'book_list': books}  # Create a context dictionary with book list
       return render(request, 'relationship_app/list_books.html', context)
+
+class LibraryDetailView(DetailView):
+  """A class-based view for displaying details of a specific book."""
+  model = Library
+  template_name = 'relationship_app/book_detail.html'
+
+  def get_context_data(self, **kwargs):
+    """Injects additional context data specific to the book."""
+    context = super().get_context_data(**kwargs)  # Get default context data
+    book = self.get_object()  # Retrieve the current book instance
+    context['average_rating'] = book.get_average_rating() 
