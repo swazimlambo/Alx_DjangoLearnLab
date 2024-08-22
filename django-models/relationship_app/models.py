e328f930a6ab81a5b47d29a5ab0fe415bf8ccb31
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 21:41:38 +0200

    roles

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -28,3 +29,12 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+
+class UserProfile(User):
+    ROLES = [
+        ('Admin', 'Admin'),
+        ('Member', 'Member'),
+        ('Librarian', 'Librarian')
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLES, default='Member')
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,5 +1,13 @@
 from .views import list_books, LibraryDetailView
 
-views.register 
-LogoutView.as_view(template_name= logout)
-LoginView.as_view(template_name= login)
+#views.register 
+#LogoutView.as_view(template_name= logout)
+#LoginView.as_view(template_name= login)#
+
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='relationship_app//login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+]
