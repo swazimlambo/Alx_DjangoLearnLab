c565666155965bcf264b7f7c84000173fd238428
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 20:40:48 +0200

    detail

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.views.generic import DetailView
 from django.shortcuts import render
 from .models import Book
 from .models import Library
+from django.views.generic.detail import DetailView
 
 def book_list(request):
       """Retrieves all books and renders a template displaying the list."""
