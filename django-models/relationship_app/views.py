bcff609e816f9d5a9dff98e17025bf88138bb297
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 20:56:32 +0200

    views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 def list_books(request):
       """Retrieves all books and renders a template displaying the list."""
@@ -20,3 +23,8 @@ class LibraryDetailView(DetailView):
     context = super().get_context_data(**kwargs)  # Get default context data
     book = self.get_object()  # Retrieve the current book instance
     context['average_rating'] = book.get_average_rating() 
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/registration.html'
