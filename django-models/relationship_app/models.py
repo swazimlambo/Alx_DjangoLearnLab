323e480e50cf9772327612a91ce212074e2e60e9
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 20:05:02 +0200

    models

---
+++ django-models/relationship_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)    
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+class Library(models.Model):
+    name = models.CharField(max_length=300)
+    books = models.ManyToManyField(Book,related_name='library')
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=500)    
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
