c010b1552ed646b146408d8d007e6100c67eb6a4
Swazi Mlambo <swaziimlambo@gmail.com>
2024-08-22 20:37:31 +0200

    library

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -11,7 +11,7 @@ def book_list(request):
 class LibraryDetailView(DetailView):
   """A class-based view for displaying details of a specific book."""
   model = Library
-  template_name = 'relationship_app/book_detail.html'
+  template_name = 'relationship_app/library_detail.html'
 
   def get_context_data(self, **kwargs):
     """Injects additional context data specific to the book."""
