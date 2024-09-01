9825883c393257211aeb97aa353422525729ffa7
Swazi Mlambo <swaziimlambo@gmail.com>
2024-09-01 09:40:00 +0200

    views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,6 +3,6 @@ from .models import Book
 from django.shortcuts import render
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def list_books(request):
+def book_list(request):
     book = Book.objects.all()
-    return render(request, 'bookshelf/list_books.html', {'books': book})
+    return render(request, 'bookshelf/book_list.html', {'books': book})
