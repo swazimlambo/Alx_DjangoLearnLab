1565fcb06d5fee85ff92b98880beaad02993d778
Swazi Mlambo <swaziimlambo@gmail.com>
2024-09-05 09:35:12 +0200

    thi

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from django.shortcuts import render
+from .forms import ExampleForm
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
