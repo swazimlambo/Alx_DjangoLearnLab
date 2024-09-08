2d15ffe7600f4e64818425f2ab14360063176f18
Swazi Mlambo <swaziimlambo@gmail.com>
2024-09-08 17:31:32 +0200

    first

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=20)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.name
