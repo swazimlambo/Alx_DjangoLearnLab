03e4986bc39c287889690597d5d13bb3cfec79af
Swazi Mlambo <swaziimlambo@gmail.com>
2024-09-08 23:17:46 +0200

    secon

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,8 +10,8 @@ urlspatterns = [
     path('api/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
     path('/books/', BookListView.as_view(), name='book-list'),
-    path('/books/<cre:pk>/', BookCreateView.as_view(), name='book-create'),
-    path('/books/<del:pk>/', BookDeleteView.as_view(), name='book-delete'),
-    path('/books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('/books/<upt:pk>/', BookUpdateView.as_view(), name='book-update')
+    path('books/create', BookCreateView.as_view(), name='book-create'),
+    path('books/delete', BookDeleteView.as_view(), name='book-delete'),
+    path('books/details', BookDetailView.as_view(), name='book-detail'),
+    path('books/update', BookUpdateView.as_view(), name='book-update')
 ]
