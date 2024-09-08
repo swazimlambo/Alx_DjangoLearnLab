1a8831944b385dc759a5e43e8242a9d61e3763f1
Swazi Mlambo <swaziimlambo@gmail.com>
2024-09-08 23:21:13 +0200

    seco

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,20 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework import generics
-from rest_framework.permissions import IsAdminUser, IsAuthenticated, IsReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializers import BookSerializer
 from .models import Book
 from rest_framework.authentication import TokenAuthentication
 from .permissions import IsAuthorOrReadOnly
 
 class BookListView(generics.ListAPIView):
-    permissions_classes = [IsReadOnly]
+    permissions_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permissions_classes = [IsAdminUser]
 
 class BookDetailView(generics.DetailAPIView):
-    permission_classes = [IsReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
